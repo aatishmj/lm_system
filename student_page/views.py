af0db0d74c0e7c701ac6b4933f7b8482cbd43ffc
aatish jawalkar <37588527+aatishmj@users.noreply.github.com>
2023-07-24 13:44:57 +0530

    modified:   db.sqlite3 	modified:   student_page/__pycache__/views.cpython-310.pyc 	modified:   student_page/views.py

--- student_page/views.py
+++ student_page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponse , redirect
+from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout , login
