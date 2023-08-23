87d9be8b32400eb11439ecd406b158b2fb3905b0
aatish <aatishjawalkar13@gmail.com>
2023-08-23 11:38:36 +0530

    modified:   db.sqlite3 	modified:   lab_Management/__pycache__/urls.cpython-310.pyc 	modified:   lab_Management/urls.py 	modified:   lab_admin/__pycache__/urls.cpython-310.pyc 	modified:   lab_admin/__pycache__/views.cpython-310.pyc 	modified:   templates/admin_dash.html

--- lab_Management/urls.py
+++ lab_Management/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("student", include("student_page.urls")),
     path("", include("student_page.urls")),
     path("admindash" , include("lab_admin.urls")),
-    path("add", include("lab_admin.urls")),
+    #path("add", include("lab_admin.urls")),
 ]
