78bc94ad0bba7e7917d561a276c3319ccdeb7696
aatishmj <aatishjawalkar13@gmail.com>
2023-10-24 13:54:30 +0530

    modified:   .DS_Store 	modified:   db.sqlite3 	new file:   lab_Management/__pycache__/__init__.cpython-311.pyc 	new file:   lab_Management/__pycache__/settings.cpython-311.pyc 	new file:   lab_Management/__pycache__/urls.cpython-311.pyc 	new file:   lab_Management/__pycache__/wsgi.cpython-311.pyc 	new file:   lab_admin/__pycache__/__init__.cpython-311.pyc 	new file:   lab_admin/__pycache__/admin.cpython-311.pyc 	new file:   lab_admin/__pycache__/apps.cpython-311.pyc 	new file:   lab_admin/__pycache__/models.cpython-311.pyc 	new file:   lab_admin/__pycache__/urls.cpython-311.pyc 	new file:   lab_admin/__pycache__/views.cpython-311.pyc 	new file:   lab_admin/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   lab_admin/migrations/__pycache__/0002_ticket.cpython-311.pyc 	new file:   lab_admin/migrations/__pycache__/0003_h_ticket_s_ticket.cpython-311.pyc 	new file:   lab_admin/migrations/__pycache__/__init__.cpython-311.pyc 	new file:   static/loginstyle.css 	deleted:    static/sign-in.css 	new file:   static/sign1style.css 	new file:   student_page/__pycache__/__init__.cpython-311.pyc 	new file:   student_page/__pycache__/admin.cpython-311.pyc 	new file:   student_page/__pycache__/apps.cpython-311.pyc 	new file:   student_page/__pycache__/models.cpython-311.pyc 	new file:   student_page/__pycache__/urls.cpython-311.pyc 	new file:   student_page/__pycache__/views.cpython-311.pyc 	new file:   student_page/migrations/__pycache__/__init__.cpython-311.pyc 	modified:   student_page/views.py 	new file:   templates/.DS_Store 	modified:   templates/dboard.html 	new file:   templates/login.html 	modified:   templates/sign.html

--- student_page/views.py
+++ student_page/views.py
@@ -16,9 +16,9 @@ def login_user(request):
             login(request, user)
             return redirect("dboard")
         else:
-            return render(request ,"s_log.html")
+            return render(request ,"login.html")
 
-    return render(request ,"s_log.html")
+    return render(request ,"login.html")
 
 def sign_up(request):
     if request.method == 'POST':
